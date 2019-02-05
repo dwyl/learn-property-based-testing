fb8df2d97f0980a7cafd75246d2a96678a143a10
Danielwhyte <danwhy@gmail.com>
2019-02-05 12:33:27 +0000

    adds example code and tests

--- example/lib/frame.ex
+++ example/lib/frame.ex
@@ -1,18 +1,46 @@
 defmodule Frame do
   @moduledoc """
-  Documentation for Frame.
+    Displays a list of strings
+    within a 'frame' of asterisks
   """
+  def run(input) do
+    input
+    |> build()
+    |> print()
+  end
 
-  @doc """
-  Hello world.
+  def build(input) do
+    longest = Enum.max_by(input, &String.length/1) |> String.length()
 
-  ## Examples
+    top = String.duplicate("*", longest + 4)
 
-      iex> Frame.hello()
-      :world
+    input
+    |> validate()
+    |> Enum.map(fn s ->
+      "* " <> s <> suffix(longest - String.length(s) + 1)
+    end)
+    |> List.insert_at(0, top)
+    |> List.insert_at(-1, top)
+  end
 
-  """
-  def hello do
-    :world
+  defp validate(input) do
+    case Enum.all?(input, &is_ascii?/1) do
+      true -> input
+      false -> raise ArgumentError
+    end
+  end
+
+  defp print(frame) do
+    frame
+    |> Enum.join("\n")
+    |> IO.puts()
+  end
+
+  defp suffix(len) do
+    String.duplicate(" ", len) <> "*"
+  end
+
+  defp is_ascii?(str) do
+    str |> String.to_charlist() |> List.ascii_printable?()
   end
 end
--- example/test/frame_test.exs
+++ example/test/frame_test.exs
@@ -1,8 +1,39 @@
 defmodule FrameTest do
   use ExUnit.Case
-  doctest Frame
+  use ExUnitProperties
 
-  test "greets the world" do
-    assert Frame.hello() == :world
+  property "Frame height = length of input list + 2" do
+    check all l <- StreamData.string(:ascii) |> StreamData.list_of(min_length: 1) do
+      frame_height = l |> Frame.build() |> length
+      length_of_list = length(l)
+
+      assert frame_height == length_of_list + 2
+    end
+  end
+
+  property "Frame width = length of longest input string + 4" do
+    check all l <- StreamData.string(:ascii) |> StreamData.list_of(min_length: 1) do
+      frame_width = l |> Frame.build() |> List.first() |> String.length()
+      longest_string = Enum.max_by(l, &String.length/1) |> String.length()
+
+      assert frame_width == longest_string + 4
+    end
+  end
+
+  property "All sides are equal width" do
+    check all l <- StreamData.string(:ascii) |> StreamData.list_of(min_length: 1) do
+      frame_width = l |> Frame.build() |> List.first() |> String.length()
+
+      assert Enum.all?(Frame.build(l), fn s -> String.length(s) == frame_width end)
+    end
+  end
+
+  property "Raises error on non-ascii input" do
+    check all l <-
+                StreamData.string(:printable, min_length: 1)
+                |> filter(&(&1 not in 32..255))
+                |> StreamData.list_of(min_length: 1) do
+      assert_raise ArgumentError, fn -> Frame.build(l) end
+    end
   end
 end
